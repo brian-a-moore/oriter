2d8eaf04572e5d4eaa75679a45bc4d6dab068e7c
Brian Moore <brianmoore0213@gmail.com>
2024-02-26 12:23:49 -0500

    fix

--- terraform-backend/src/middleware/schema-validator.middleware.ts
+++ terraform-backend/src/middleware/schema-validator.middleware.ts
@@ -2,9 +2,7 @@ import { ObjectSchema } from 'joi';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 
 const schemaValidatorMiddleware = async (event: APIGatewayProxyEvent, schema: ObjectSchema) => {
-  const { body } = await schema.validateAsync({
-    body: event.body,
-  });
+  const { body } = await schema.validateAsync(event.body);
 
   event.body = body;
 
