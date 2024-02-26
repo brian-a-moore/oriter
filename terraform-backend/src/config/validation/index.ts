0f1955df1e1638175f1a56f489e8debcf7c9b05e
Brian Moore <brianmoore0213@gmail.com>
2024-02-26 13:34:08 -0500

    update

--- terraform-backend/src/config/validation/index.ts
+++ terraform-backend/src/config/validation/index.ts
@@ -1,7 +1,15 @@
-import { ObjectSchema } from 'joi';
+import Joi, { ObjectSchema } from 'joi';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 
 const schemaValidatorMiddleware = async (event: APIGatewayProxyEvent, schema: ObjectSchema) => {
+  if(!event.body) {
+    throw new Joi.ValidationError('Body undefined', [{
+      message: 'Body undefined',
+      path: ['body'],
+      type: 'error',
+    }], []);
+  }
+  
   const { body } = await schema.validateAsync(event.body);
 
   event.body = body;
