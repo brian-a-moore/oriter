545473638a1e763a04cb34ead1b11c4b92fbccc0
Brian Moore <brianmoore0213@gmail.com>
2024-02-26 02:14:48 -0500

    update

--- terraform-backend/.aws/main.tf
+++ terraform-backend/.aws/main.tf
@@ -93,6 +93,15 @@ resource "aws_lambda_function" "oriter_form_submission" {
   depends_on = [aws_s3_bucket_object.object]
 }
 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.oriter_form_submission.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
 # API Gateway
 resource "aws_api_gateway_rest_api" "api" {
   name        = "oriter_api"
@@ -120,4 +129,10 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.oriter_form_submission.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "v1"
 }
