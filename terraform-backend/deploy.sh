5c362542dec82035be124ee90c42eb8463053af0
Brian Moore <brianmoore0213@gmail.com>
2024-02-26 12:28:53 -0500

    update

--- terraform-backend/deploy.sh
+++ terraform-backend/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Deploy starting..."
+
 # Run the build command
 npm run build
 
@@ -9,8 +11,7 @@ cd .aws && terraform apply
 # Go back to the project root directory
 cd ..
 
-# Print the contents of the zip file
-unzip -l oriter_code.zip
-
 # Remove the dist folder and the zip file
 rm -rf dist oriter_code.zip
+
+echo "Deploy complete"
