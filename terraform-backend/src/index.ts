17abad5bf150225a10b79d7252c3960feaa1ffd4
Brian Moore <brianmoore0213@gmail.com>
2024-02-26 09:56:03 -0500

    update

--- terraform-backend/src/index.ts
+++ terraform-backend/src/index.ts
@@ -1,26 +1,38 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import middy from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
-import jsonBodyParser from '@middy/http-json-body-parser';
 import * as routes from './functions';
 
 const handler = async (event: APIGatewayProxyEvent, context: Context) => {
   console.log('event', event);
   console.log('context', context);
-  switch (event.resource) {
-    case 'POST /login':
-      return routes.login(event, context);
-    case 'POST /register':
-      return routes.register(event, context);
-    case 'POST /submit':
-      return routes.submit(event, context);
-    case 'POST /verifyFormLink':
-      return routes.verifyFormLink(event, context);
-    case 'POST /verifyToken':
-      return routes.verifyToken(event, context);
-    default:
-      return { statusCode: 400, body: 'Invalid route' };
+
+  // Parse JSON body
+  if (event.body) {
+    event.body = JSON.parse(event.body);
+  }
+
+  try {
+    switch (event.resource) {
+      case 'POST /login':
+        return routes.login(event, context);
+      case 'POST /register':
+        return routes.register(event, context);
+      case 'POST /submit':
+        return routes.submit(event, context);
+      case 'POST /verifyFormLink':
+        return routes.verifyFormLink(event, context);
+      case 'POST /verifyToken':
+        return routes.verifyToken(event, context);
+      default:
+        return { statusCode: 400, body: 'Invalid route' };
+    }
+  } catch (error: any | unknown) {
+    // Handle errors
+    console.error(error);
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ message: error.message || 'Internal Server Error' }),
+    };
   }
 };
 
-export const handlerMiddleware = middy(handler).use(jsonBodyParser()).use(httpErrorHandler());
+export const handlerMiddleware = handler;
