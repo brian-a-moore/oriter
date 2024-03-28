f047cfb2d383728e253b692152ee23d145a0722e
Brian Moore <brianmoore0213@gmail.com>
2024-03-28 04:46:15 -0400

    updates

--- terraform-backend/deploy.sh
+++ terraform-backend/deploy.sh
@@ -2,28 +2,16 @@
 
 echo "Deploy starting..."
 
-# Run Prisma migration
-echo "Step 1 of 4: Running Prisma migration..."
-dotenv -e .env -- npx prisma migrate deploy
-dotenv -e .env -- npx prisma generate
+echo "Building functions..."
+cd ./functions/api && npm run build
+cp -r ./prisma ../migrations
+cd ../migrations && npm run build
 
-echo "Step 1 of 4: COMPLETE"
-# Run the build command
-echo "Step 2 of 4: Building..."
-npm run build
+echo "Deploying infrastructure..."
+cd ../../.aws && terraform apply -auto-approve
 
-echo "Step 2 of 4: COMPLETE"
-# Run the deploy command
-echo "Step 3 of 4: Deploying..."
-cd .aws && terraform apply -auto-approve
+echo "Cleaning up artifacts..."
+cd ../functions/api && rm -rf dist oriter_api.zip
+cd ../migrations && rm -rf dist prisma oriter_migrations.zip
 
-echo "Step 3 of 4: COMPLETE"
-# Go back to the project root directory
-echo "Step 4 of 4: Cleanup..."
-cd ..
-
-# Remove the dist folder and the zip file
-rm -rf dist oriter_code.zip
-
-echo "Step 4 of 4: COMPLETE"
 echo "Deploy COMPLETE"
