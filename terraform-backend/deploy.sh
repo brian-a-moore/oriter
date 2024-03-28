9f6a9d1ac308937015d613e40467df78ba686398
Brian Moore <brianmoore0213@gmail.com>
2024-03-28 04:55:18 -0400

    small update

--- terraform-backend/deploy.sh
+++ terraform-backend/deploy.sh
@@ -14,4 +14,7 @@ echo "Cleaning up artifacts..."
 cd ../functions/api && rm -rf dist oriter_api.zip
 cd ../migrations && rm -rf dist prisma oriter_migrations.zip
 
+echo "Running migrations..."
+aws lambda invoke --function-name migrations_lambda
+
 echo "Deploy COMPLETE"
