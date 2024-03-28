ef8c253609ce99c0fe17050fac144635bbb6c8ad
Brian Moore <brianmoore0213@gmail.com>
2024-03-27 23:05:33 -0400

    fixes

--- terraform-backend/src/helpers/imageHandler.ts
+++ terraform-backend/src/helpers/imageHandler.ts
@@ -6,14 +6,13 @@ import { ENV_TYPE } from '../constants';
 
 const s3 = new S3Client({ region: process.env.AWS_REGION });
 
-const dirPath = path.join(__dirname, '../images');
-
 export default async (files: File[], lovedOneId: string) => {
+  const dirPath = path.join(__dirname, `../images/${lovedOneId}`);
   if (process.env.APP_ENV === ENV_TYPE.LOCAL) {
     await fs.promises.mkdir(dirPath, { recursive: true });
     files.forEach(async (file) => {
       const oldPath = file.filepath;
-      const newPath = path.join(dirPath, `${lovedOneId}/${file.originalFilename}`);
+      const newPath = path.join(dirPath, file.originalFilename as string);
 
       await fs.promises.rename(oldPath, newPath);
     });
@@ -29,7 +28,6 @@ export default async (files: File[], lovedOneId: string) => {
 
       const command = new PutObjectCommand(params);
       await s3.send(command);
-      console.log(`File uploaded successfully: ${file.originalFilename}`);
     }
   }
 };
